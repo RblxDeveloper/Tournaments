818ef6b6ef0ebddc93fdf59cb02c6149520ad226
ProScripter <110280452+RblxDeveloper@users.noreply.github.com>
2023-06-03 17:26:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-document.getElementById('signup-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-  
-    // Get the username value
-    var usernameInput = document.getElementById('username');
-    var username = usernameInput.value.trim();
-  
-    if (username !== '') {
-      // Create a new list item with the username
-      var listItem = document.createElement('li');
-      listItem.textContent = username;
-  
-      // Append the list item to the participant list
-      var participantList = document.getElementById('participant-list');
-      participantList.appendChild(listItem);
-  
-      // Clear the username input field
-      usernameInput.value = '';
-    }
+// Function to save the participant list to local storage
+function saveParticipantList(participants) {
+  localStorage.setItem('participants', JSON.stringify(participants));
+}
+
+// Function to load the participant list from local storage
+function loadParticipantList() {
+  var participants = localStorage.getItem('participants');
+  return participants ? JSON.parse(participants) : [];
+}
+
+// Function to update the participant list in the UI
+function updateParticipantList() {
+  var participantList = document.getElementById('participant-list');
+  participantList.innerHTML = '';
+
+  var participants = loadParticipantList();
+  participants.forEach(function (username) {
+    var listItem = document.createElement('li');
+    listItem.textContent = username;
+    participantList.appendChild(listItem);
   });
+}
+
+// Event listener for form submission
+document.getElementById('signup-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var usernameInput = document.getElementById('username');
+  var username = usernameInput.value.trim();
+
+  if (username !== '') {
+    var participants = loadParticipantList();
+    participants.push(username);
+    saveParticipantList(participants);
+
+    updateParticipantList();
+
+    usernameInput.value = '';
+  }
+});
+
+// Load the participant list on page load
+updateParticipantList();
